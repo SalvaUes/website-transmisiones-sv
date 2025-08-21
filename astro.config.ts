2283ebdac65ada87ba9b7da668fbbdfe24101945
SalvaUes <VL23003@ues.edu.sv>
2025-08-20 22:25:43 -0600

    FIX: Cambiar formato de build de Astro a 'file'

--- astro.config.ts
+++ astro.config.ts
@@ -3,6 +3,10 @@ import sanity from "@sanity/astro";
 import icon from "astro-icon";
 
 export default defineConfig({
+  // ---> AÑADE ESTA SECCIÓN COMPLETA <---
+  build: {
+    format: 'file'
+  },
   integrations: [
     sanity({
       projectId: 'xr5j5141', // Asegúrate de que tu ID sigue aquí
